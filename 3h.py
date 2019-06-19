f08fe1d71dbc789f4063a8db14bbaf10e2cf2703
deepanshutayal <deepanshutayal@github.com>
2019-06-19 21:56:05 +0530

    Create 3h.py

---
+++ 3h.py
@@ -0,0 +1,13 @@
+a=int(input('enter 1st numbers'))
+j=int(input('enter 2nd numbers'))
+i=int(input('enter 3rd numbers'))
+h=int(input('enter 4th numbers'))
+g=int(input('enter 5th numbers'))
+f=int(input('enter 6th numbers'))
+e=int(input('enter 7th numbers'))
+d=int(input('enter 8th numbers'))
+c=int(input('enter 9th numbers'))
+b=int(input('enter 10th numbers'))
+
+sum=a+b+c+d+e+f+g+h+i+j
+print(sum)
