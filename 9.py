c4a931545743fa921cb1afcb9259358d496caa52
deepanshutayal <deepanshutayal@github.com>
2019-06-21 09:52:20 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,9 @@
+m=float(input('enter distance between two cities in km '))
+i=m*39370.0787
+f=m*3280.839
+c=m*100000
+mt=m*1000
+print('in meters ',mt)
+print('in inches ',i)
+print('in centimeter ',c)
+print('in feet ',f)
