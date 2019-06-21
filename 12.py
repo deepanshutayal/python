828b807033f114b2b157a38c5fec613bfd0e76c4
deepanshutayal <deepanshutayal@github.com>
2019-06-21 09:51:50 +0530

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,23 @@
+m=int(input('enter five digit no. '))
+y=0
+x=m%10
+y=y*10+x
+m=m//10
+
+x=m%10
+y=y*10+x
+m=m//10
+
+x=m%10
+y=y*10+x
+m=m//10
+
+x=m%10
+y=y*10+x
+m=m//10
+
+x=m%10
+y=y*10+x
+
+
+print('reverse of a no. ',y)
