b8dd42b521ce623657c68df91de87ebc4d2e9bf4
deepanshutayal <deepanshutayal@github.com>
2019-06-21 09:51:38 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,10 @@
+m=float(input('enter first subject marks '))
+n=float(input('enter second subject marks '))
+o=float(input('enter third subject marks '))
+p=float(input('enter fourth subject marks '))
+q=float(input('enter fifth subject marks '))
+t=m+n+o+p+q
+a=t/5
+p=(t*100)/500
+print('agregate marks ',a)
+print('percantage ',p,'%')
